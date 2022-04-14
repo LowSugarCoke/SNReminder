12c5e8f5ea2c466851e8426c72f89b123acba69c
Jack Lee <jack20499@gmail.com>
2022-04-14 10:59:08 +0800

    connect ui

--- sn_reminder.py
+++ sn_reminder.py
@@ -1,6 +1,45 @@
 import sn_lib
+import sn_reminder_manager
 import tkinter as tk
 from tkinter import ttk
+
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.geometry('800x500')
+        self.entry_collect_anime = tk.Entry(
+            self, width=35, font=('Arial 24')).grid(row=0, column=0, padx=20, pady=20)
+        self.button_collect = tk.Button(self, height=2, text='收藏動漫', command=self.connect_collect_button).grid(
+            row=0, column=1, padx=20, pady=20)
+
+        self.listbox = tk.Listbox(self, width=70, height=20, font=('Arial 12'))
+        for i in range(200):
+            self.listbox.insert('end', i)
+        self.listbox.grid(row=1, rowspan=3, column=0)
+
+        self.button_update_list = tk.Button(self, height=2, text='更新動漫列表', command=self.connect_update_button).grid(
+            row=1, column=1, padx=20, sticky=tk.N)
+        self.button_collect_list = tk.Button(self, height=2, text='收藏動漫列表', command=self.connect_collect_list_button).grid(
+            row=1, column=1, padx=20, )
+
+        self.checkbutton_reminder = tk.Checkbutton(
+            self).grid(row=3, column=1, padx=20, sticky=(tk.W, tk.S))
+
+        self.label_reminder = tk.Label(self, text="每日提醒").grid(
+            row=3, column=1, padx=40, ipady=3, sticky=(tk.W, tk.S))
+
+    def connect_collect_button(self):
+        print("連接收藏動漫")
+
+    def connect_update_button(self):
+        print("連接更新動漫")
+
+    def connect_collect_list_button(self):
+        print("連接收藏動漫更新")
 
 
 if __name__ == '__main__':
@@ -32,26 +71,6 @@ if __name__ == '__main__':
     #             SNLib.read_anime()
     #         SNLib.print_local_anime()
 
-    root = tk.Tk()
-    root.geometry('800x500')
-    entry_collect_anime = tk.Entry(
-        root, width=35, font=('Arial 24')).grid(row=0, column=0, padx=20, pady=20)
-    button_collect = tk.Button(root, height=2, text='收藏動漫').grid(
-        row=0, column=1, padx=20, pady=20)
-
-    listbox = tk.Listbox(root, width=70, height=20, font=('Arial 12'))
-    for i in range(200):
-        listbox.insert('end', i)
-    listbox.grid(row=1, rowspan=3, column=0)
-
-    button_update_list = tk.Button(root, height=2, text='更新動漫列表').grid(
-        row=1, column=1, padx=20, sticky=tk.N)
-    button_collect_list = tk.Button(root, height=2, text='收藏動漫列表').grid(
-        row=1, column=1, padx=20, )
-
-    checkbutton_reminder = tk.Checkbutton(
-        root).grid(row=3, column=1, padx=20, sticky=(tk.W, tk.S))
-
-    label_reminder = tk.Label(root, text="每日提醒").grid(
-        row=3, column=1, padx=40, ipady=3, sticky=(tk.W, tk.S))
-    root.mainloop()
+    app = Application()
+    app.title("SN Reminder")
+    app.mainloop()
